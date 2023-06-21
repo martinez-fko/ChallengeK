2e1f2991930353f37c5b600907f1cc2be516df2c
Francisco Martínez <jose_fko_92@hotmail.com>
2023-06-21 12:51:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
   const handleResizeStart = (index, e) => {
     console.log("e", e.direction);
     // Check if the resize is coming from the left handle
-    const [handlePosX, handlePosY] = e.direction;
+    const [handlePosX] = e.direction;
     // 0 => center
     // -1 => top or left
     // 1 => bottom or right
@@ -64,8 +64,6 @@ const App = () => {
     if (handlePosX === -1) {
       console.log("width", moveableComponents, e);
       // Save the initial left and width values of the moveable component
-      const initialLeft = e.left;
-      const initialWidth = e.width;
 
       // Set up the onResize event handler to update the left value based on the change in width
     }
